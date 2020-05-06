4df58d93f54bdb0135a0165d20b4ae7bf6f55257
Larry Fritts <larry.fritts@linuxacademy.com>
2020-05-06 09:56:41 -0700

    ready for record

---
+++ hol/write_config.py
@@ -0,0 +1,42 @@
+import configparser
+
+# data to be used
+data_list = [
+    ["author", "title", "pages", "due_date", "genre", "isbn"],
+    ["Thompson, Keith", "Oh Python! My Python!", 1200, "2020-11-15", "biography", "000-1-000000-00-1"],
+    ["Fritts, Larry", "Fun with Django", 150, "2020-06-23", "satire", "00-2-000000-00-2"]
+]
+
+novel_text = "Numquam labore labore tempora. Dolore dolorem quisquam aliquam eius dolor non ipsum. Dolorem est velit tempora quaerat. Est magnam porro voluptatem dolore. Adipisci voluptatem consectetur dolore voluptatem voluptatem neque. Adipisci non modi quiquia etincidunt quisquam. Etincidunt consectetur dolore ut. Quaerat ut adipisci sit aliquam quisquam ut. Voluptatem labore porro porro.\n\nPorro modi numquam non sed dolor. Consectetur dolor adipisci quaerat aliquam adipisci. Etincidunt porro modi dolor neque numquam magnam neque. Ipsum consectetur ut eius ut. Dolor ut modi non est tempora labore sit.\n\nEtincidunt voluptatem quaerat consectetur sed est etincidunt ipsum. Ut adipisci numquam etincidunt porro porro. Dolore eius modi aliquam. Velit amet sed quaerat etincidunt est. Ut consectetur modi ipsum sed aliquam. Amet tempora quiquia eius voluptatem aliquam dolore ut. Etincidunt dolor non voluptatem eius sed non adipisci."
+
+# instantiate the configparser object
+config = configparser.ConfigParser()
+
+# add default section
+config["DEFAULT"] = {
+    "pub_house": "Atlantic Publishing",
+    "number_of_books": '2'
+}
+
+# add first book
+config["book1"] = {
+    "author": data_list[1][0],
+    "title": data_list[1][1],
+    "genre": data_list[1][4],
+    "text": novel_text
+}
+
+# add second book
+config["book2"] = {
+    "author": data_list[2][0],
+    "title": data_list[2][1],
+    "genre": data_list[2][4],
+    "text": novel_text
+}
+
+# print to file
+with open("printer_config.ini", "w") as f:
+    config.write(f)
+
+# test
+# student input here
