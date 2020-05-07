089bb6cb2e249abaa7657dd6d3495c93020a9584
Larry Fritts <larry.fritts@linuxacademy.com>
2020-05-07 08:05:27 -0700

    ready for record

--- printer_file.py
+++ printer_file.py
@@ -9,14 +9,9 @@ data_list = [
 
 novel_text = "Numquam labore labore tempora. Dolore dolorem quisquam aliquam eius dolor non ipsum. Dolorem est velit tempora quaerat. Est magnam porro voluptatem dolore. Adipisci voluptatem consectetur dolore voluptatem voluptatem neque. Adipisci non modi quiquia etincidunt quisquam. Etincidunt consectetur dolore ut. Quaerat ut adipisci sit aliquam quisquam ut. Voluptatem labore porro porro.\n\nPorro modi numquam non sed dolor. Consectetur dolor adipisci quaerat aliquam adipisci. Etincidunt porro modi dolor neque numquam magnam neque. Ipsum consectetur ut eius ut. Dolor ut modi non est tempora labore sit.\n\nEtincidunt voluptatem quaerat consectetur sed est etincidunt ipsum. Ut adipisci numquam etincidunt porro porro. Dolore eius modi aliquam. Velit amet sed quaerat etincidunt est. Ut consectetur modi ipsum sed aliquam. Amet tempora quiquia eius voluptatem aliquam dolore ut. Etincidunt dolor non voluptatem eius sed non adipisci."
 
-# instantiate the configparser object and read file
-
-
-# add isbn for books
-
-
-# print to file
-# overwrite old file
+# use configparser to create the configuration file as described in the instructions
+# write the file to printer_config.ini
+pass # student code here
 
 
 # test
@@ -26,8 +21,7 @@ try:
 except FileNotFoundError:
         config_text = ""
 
-with open("test_isbn.ini", "r") as f:
+with open("test_printer_config.ini", "r") as f:
     expected_text = f.read()
 
 assert config_text == expected_text
-
--- update_printer_file.py
+++ update_printer_file.py
@@ -9,29 +9,18 @@ data_list = [
 
 novel_text = "Numquam labore labore tempora. Dolore dolorem quisquam aliquam eius dolor non ipsum. Dolorem est velit tempora quaerat. Est magnam porro voluptatem dolore. Adipisci voluptatem consectetur dolore voluptatem voluptatem neque. Adipisci non modi quiquia etincidunt quisquam. Etincidunt consectetur dolore ut. Quaerat ut adipisci sit aliquam quisquam ut. Voluptatem labore porro porro.\n\nPorro modi numquam non sed dolor. Consectetur dolor adipisci quaerat aliquam adipisci. Etincidunt porro modi dolor neque numquam magnam neque. Ipsum consectetur ut eius ut. Dolor ut modi non est tempora labore sit.\n\nEtincidunt voluptatem quaerat consectetur sed est etincidunt ipsum. Ut adipisci numquam etincidunt porro porro. Dolore eius modi aliquam. Velit amet sed quaerat etincidunt est. Ut consectetur modi ipsum sed aliquam. Amet tempora quiquia eius voluptatem aliquam dolore ut. Etincidunt dolor non voluptatem eius sed non adipisci."
 
-# instantiate the configparser object
+# use configparser to change the configuration file as described in the instructions
+# overwrite printer_config.ini
+pass # student code here
 
-
-# add default section
-
-
-# add first book
-
-
-# add second book
-
-
-# print to file
-
-
-# test
 try:
     with open("printer_config.ini", "r") as f:
         config_text = f.read()
 except FileNotFoundError:
         config_text = ""
 
-with open("test_printer_config.ini", "r") as f:
+with open("test_isbn.ini", "r") as f:
     expected_text = f.read()
 
 assert config_text == expected_text
+
